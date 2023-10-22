413d5310b25585f7e186deecc43e4fa32725f8b3
Adamer15 <raczkowski96@gmail.com>
2023-10-22 14:30:17 +0200

    solved no output in terminal from zadanie_3()

--- zadanie_3.c
+++ zadanie_3.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <math.h>
-#define przerwa() "##############################"
+#define przerwa() printf("\n##############################\n")
 void zadanie_3a()
 {
     double a, b, s;
+    printf("Podaj dwie liczby: ");
     scanf("%lf %lf", &a, &b);
     if (a > b)
         printf("Pierwsza liczba jest wieksza od drugiej");
@@ -12,11 +13,12 @@ void zadanie_3a()
     else
         printf("Druga liczba jest wieksza od pierwszej");
     s = a + b;
-    printf("\nSuma wprowadzonych liczb wynosi: %.2lf", s);
+    printf("\nSuma wprowadzonych liczb wynosi: %.2lf\n", s);
 }
 void zadanie_3b()
 {
     double n = 0, a, b, c, srednia, suma;
+    printf("\nPodaj trzy liczby: ");
     scanf("%lf", &a);
     n++;
     scanf("%lf", &b);
@@ -27,26 +29,27 @@ void zadanie_3b()
     srednia = suma/n;
     if (srednia > 5)
         printf("Wysoka srenia ");
-    printf("%.2lf %.2lf", srednia, suma);
+    printf("%.2lf %.2lf\n", srednia, suma);
 }
 void zadanie_3c()
 {
     double a, b, c, delta = 0, p, q, x1, x2;
+    printf("\nPodaj wspolczynniki trojmianu kwadratowego: ");
     scanf("%lf %lf %lf", &a, &b, &c);
     delta = b*b - (4*a*c);
     p = (-b)/(2*a);
     q = (-delta)/(4*a);
     x1 = ((-b)+sqrt(delta))/(2*a);
     x2 = ((-b)-sqrt(delta))/(2*a);
-    printf("Wierzcholek funkcji to: W(%.2lf, %.2lf). Miejsca zerowe: x1 = %.2lf, x2 = %.2lf", p, q, x1, x2);
+    printf("Wierzcholek funkcji to: W(%.2lf, %.2lf). Miejsca zerowe: x1 = %.2lf, x2 = %.2lf\n", p, q, x1, x2);
 }
 void zadanie_3d()
 {
     char c;
-    double n, k;
-    printf("Podaj znak i liczbe poziomow: ");
-    scanf("%c %lf", &c, &n);
-
+    double n;
+    printf("\nPodaj znak i liczbe poziomow: ");
+    scanf(" %c %lf", &c, &n);
+    getchar();
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= i; j++)
             printf("%c", c);
